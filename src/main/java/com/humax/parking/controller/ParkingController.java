0f538f581b96165db697c0579dcee41bb51d9301
CYJhub <yejee1018@naver.com>
2023-11-22 10:25:57 +0900

    refactor: update 리팩토링

--- src/main/java/com/humax/parking/controller/ParkingController.java
+++ src/main/java/com/humax/parking/controller/ParkingController.java
@@ -21,7 +21,6 @@ public class ParkingController {
 
     private final ParkingRepository parkingRepository;
 
-
     @PostMapping("/create")
     public ResponseEntity<String> createParkingInfo(@RequestBody ParkingDTO parkingDTO) {
         parkingService.createParkingInfo(parkingDTO);
@@ -33,7 +32,6 @@ public class ParkingController {
         return ResponseEntity.status(HttpStatus.OK).body(parkingService.getParkingInfo());
     }
 
-
     @GetMapping("/read/detail/{parking_id}")
     public ResponseEntity<ParkingDTO> getParkingDetail(@PathVariable Long parking_id){
         return ResponseEntity.status(HttpStatus.OK).body(parkingService.getParkingDetail(parking_id));
@@ -43,37 +41,13 @@ public class ParkingController {
     public ResponseEntity<ParkingDTO> updateParking(@RequestBody ParkingDTO parkingDTO){
         ParkingEntity existingParking = parkingRepository.findByParkingId(parkingDTO.getParkingId()).orElse(null);
         if(existingParking != null){
-            existingParking.setCodeNumber(Optional.ofNullable(parkingDTO.getCodeNumber()).orElse(existingParking.getCodeNumber()));
-            existingParking.setName(Optional.ofNullable(parkingDTO.getName()).orElse(existingParking.getName()));
-            existingParking.setAddress(Optional.ofNullable(parkingDTO.getAddress()).orElse(existingParking.getAddress()));
-            existingParking.setLat(Optional.ofNullable(parkingDTO.getLat()).orElse(existingParking.getLat()));
-            existingParking.setLon(Optional.ofNullable(parkingDTO.getLon()).orElse(existingParking.getLon()));
-            existingParking.setOperatingTime(Optional.ofNullable(parkingDTO.getOperatingTime()).orElse(existingParking.getOperatingTime()));
-            existingParking.setNormalSeason(Optional.ofNullable(parkingDTO.getNormalSeason()).orElse(existingParking.getNormalSeason()));
-            existingParking.setTenantSeason(Optional.ofNullable(parkingDTO.getTenantSeason()).orElse(existingParking.getTenantSeason()));
-            existingParking.setTimeTicket(Optional.ofNullable(parkingDTO.getTimeTicket()).orElse(existingParking.getTimeTicket()));
-            existingParking.setDayTicket(Optional.ofNullable(parkingDTO.getDayTicket()).orElse(existingParking.getDayTicket()));
-            existingParking.setSpecialDay(Optional.ofNullable(parkingDTO.getSpecialDay()).orElse(existingParking.getSpecialDay()));
-            existingParking.setSpecialHour(Optional.ofNullable(parkingDTO.getSpecialHour()).orElse(existingParking.getSpecialHour()));
-            existingParking.setSpecialNight(Optional.ofNullable(parkingDTO.getSpecialNight()).orElse(existingParking.getSpecialNight()));
-            existingParking.setSpecialWeekend(Optional.ofNullable(parkingDTO.getSpecialWeekend()).orElse(existingParking.getSpecialWeekend()));
-            existingParking.setApplyHour(Optional.ofNullable(parkingDTO.getApplyHour()).orElse(existingParking.getApplyHour()));
-            existingParking.setApplyNight(Optional.ofNullable(parkingDTO.getApplyNight()).orElse(existingParking.getApplyNight()));
-            existingParking.setApplyWeekend(Optional.ofNullable(parkingDTO.getApplyWeekend()).orElse(existingParking.getApplyWeekend()));
-            existingParking.setIsActive(Optional.ofNullable(parkingDTO.getIsActive()).orElse(existingParking.getIsActive()));
-            existingParking.setOperation(Optional.ofNullable(parkingDTO.getOperation()).orElse(existingParking.getOperation()));
-            existingParking.setCreatedAt(Optional.ofNullable(parkingDTO.getCreatedAt()).orElse(existingParking.getCreatedAt()));
-            existingParking.setDeletedAt(Optional.ofNullable(parkingDTO.getDeletedAt()).orElse(existingParking.getDeletedAt()));
-            existingParking.setTime(Optional.ofNullable(parkingDTO.getTime()).orElse(existingParking.getTime()));
-            existingParking.setPrice(Optional.ofNullable(parkingDTO.getPrice()).orElse(existingParking.getPrice()));
-
+            parkingService.updateParkingInfo(existingParking, parkingDTO);
             return new ResponseEntity<>(new ParkingDTO(parkingRepository.save(existingParking)), HttpStatus.OK);
         }
         else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 
     @DeleteMapping("/delete/{parking_id}")
     public ResponseEntity<String> deleteParking(@PathVariable Long parking_id) {
