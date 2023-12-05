87bc32b57c6c5f27ee1c1a02dc9a868ece443105
pjho4746 <pjho4746@gmail.com>
2023-12-05 10:36:14 +0900

    refactor: Deploy.yml&CorsConfig 수정

--- src/main/java/com/humax/parking/config/CorsConfig.java
+++ src/main/java/com/humax/parking/config/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.humax.parking.config;
 
-import java.util.List;
+import java.util.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,7 +13,8 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://alzzaipo.com", "http://localhost:3000"));
+        //config.setAllowedOrigins(List.of("https://alzzaipo.com", "http://localhost:3000"));
+        config.setAllowedOriginPatterns(Arrays.asList());
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
