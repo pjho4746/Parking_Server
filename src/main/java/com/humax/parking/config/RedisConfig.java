0e1a934e1e451432f6d1698018763e4f5b6dc6f9
pjho4746 <pjho4746@gmail.com>
2023-12-11 15:21:14 +0900

    refactor: redis 수정

--- src/main/java/com/humax/parking/config/RedisConfig.java
+++ src/main/java/com/humax/parking/config/RedisConfig.java
@@ -1,15 +1,22 @@
 package com.humax.parking.config;
 
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
-public class RedisConfig {
+public class RedisConfig extends CachingConfigurerSupport {
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("my-redis");
+        redisStandaloneConfiguration.setPort(6379);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
+
