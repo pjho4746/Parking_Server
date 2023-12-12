13354e6fbddaf2fb9ae5809d6cf3a16ed953969b
CYJhub <yejee1018@naver.com>
2023-12-12 17:04:37 +0900

    recover 다시

--- src/main/java/com/humax/parking/repository/EnterRepository.java
+++ src/main/java/com/humax/parking/repository/EnterRepository.java
@@ -3,8 +3,17 @@ package com.humax.parking.repository;
 import com.humax.parking.model.Bookmark;
 import com.humax.parking.model.Enter;
 import com.humax.parking.model.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface EnterRepository extends JpaRepository<Enter, Long> {
     Enter findByUser(User user);
+
+    @Modifying
+    @Query("UPDATE Enter e SET e.exitTime = :exitTime WHERE e.id = :id")
+    void updateExitTimeById(@Param("exitTime") LocalDateTime exitTime, @Param("id") Long id);
 }
