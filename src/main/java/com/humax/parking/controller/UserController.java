5d6b7449ebb78e3990123c7af99678487b79d4d9
pjho4746 <pjho4746@gmail.com>
2023-12-13 11:43:51 +0900

    feat: body변경

--- src/main/java/com/humax/parking/controller/UserController.java
+++ src/main/java/com/humax/parking/controller/UserController.java
@@ -59,17 +59,13 @@ public class UserController {
     @PostMapping("/add")
     public ResponseEntity<String> addBookmark(@RequestHeader("Authorization") String token, @RequestParam Long parkingId) {
         bookmarkService.addBookmark(token, parkingId);
-        return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body("찜 완료");
+        return ResponseEntity.status(HttpStatus.OK).body("찜 완료");
     }
 
     @PostMapping("/remove")
     public ResponseEntity<String> removeBookmark(@RequestHeader("Authorization") String token, @RequestParam Long parkingId) {
         bookmarkService.removeBookmark(token, parkingId);
-        return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body("찜 해제 완료");
+        return ResponseEntity.status(HttpStatus.OK).body("찜 해제 완료");
     }
 
     // 즐겨찾기한 주차장 리스트 조회
