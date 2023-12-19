ca3f89aaaf2c41b2e556ab241e9a598aa6b88435
CYJhub <yejee1018@naver.com>
2023-12-19 11:16:54 +0900

    parkingIdDTO 추가

--- src/main/java/com/humax/parking/dto/ParkingIdDTO.java
+++ src/main/java/com/humax/parking/dto/ParkingIdDTO.java
@@ -3,10 +3,14 @@ package com.humax.parking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ParkingIdDTO {
     private long parkingId;
+
+
 }
